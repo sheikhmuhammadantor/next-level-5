b352a151de57d7d0f4702cb8c7987d7c03876055
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2025-05-04 11:46:39 +0600

    Add TypeScript configuration and type assertion examples

---
+++ Mission 01/Typescript Technocrat/module2/dist/typeAssertion.js
@@ -0,0 +1,23 @@
+"use strict";
+{
+    // 
+    let anything = "hello !";
+    // anything = (77 as string);
+    let num1 = anything;
+    console.log(num1);
+    // Function
+    const kgToGm = (value) => {
+        if (typeof value === 'string') {
+            const convertedValue = parseFloat(value) * 1000;
+            return `The Value Is : ${convertedValue}`;
+        }
+        if (typeof value === "number") {
+            return value * 1000;
+        }
+    };
+    const result1 = kgToGm(1000);
+    const result2 = kgToGm("1000");
+    console.log(result1);
+    console.log(result2);
+    throw new Error("Hi Error !");
+}
---
+++ Mission 01/Typescript Technocrat/module2/src/typeAssertion.ts
@@ -0,0 +1,29 @@
+{
+    // 
+    let anything: unknown = "hello !";
+    // anything = (77 as string);
+    let num1 = (anything as number);
+    console.log(num1);
+
+    // Function
+    const kgToGm = (value: string | number): string | number | undefined => {
+        if (typeof value === 'string') {
+            const convertedValue = parseFloat(value) * 1000;
+            return `The Value Is : ${convertedValue}`;
+        } if (typeof value === "number") {
+            return value * 1000;
+        }
+    }
+
+    const result1 = kgToGm(1000) as number;
+    const result2 = kgToGm("1000") as string;
+    console.log(result1);
+    console.log(result2);
+
+    // Error
+    type CustomError = {
+        message: string;
+    }
+
+    throw (new Error("Hi Error !") as CustomError);
+}
