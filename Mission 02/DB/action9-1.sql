7c0da6e10963f44d9b88b5acc5fe8cfcc6033d3c
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2025-05-25 15:39:53 +0600

    Add SQL scripts for student table creation and demo data insertion

---
+++ Mission 02/DB/action9-1.sql
@@ -0,0 +1,31 @@
+-- SELECT * FROM students;
+
+-- Module 9.1;
+-- SHOW TIMEZONE;
+-- SELECT now();
+
+-- CREATE Table timeX (ts TIMESTAMP without time zone, tsz TIMESTAMP with time zone);
+-- INSERT INTO timeX VALUES('2025-09-12 10:45:00', '2025-05-22 10:45:00');
+
+-- SELECT * FROM timeX;
+
+-- SELECT CURRENT_DATE;
+-- SELECT now();
+-- SELECT now()::date;
+-- SELECT now()::time;
+
+-- SELECT to_char(now(), 'yyyy/mm/dd');
+-- SELECT to_char(now(), 'dd/mm/yyyy');
+
+-- SELECT to_char(now(), 'month');
+-- SELECT to_char(now(), 'Month');
+-- SELECT to_char(now(), 'MONTH');
+
+-- SELECT to_char(now(), 'DDD');
+-- SELECT CURRENT_DATE - INTERVAL '1 year 3 month';
+-- SELECT age(CURRENT_DATE, '2004-03-30');
+
+-- SELECT *, age(CURRENT_DATE, dob) FROM students;
+
+-- SELECT extract(MONTH from '2002-03-30'::date);
+-- SELECT 'n'::BOOLEAN;
