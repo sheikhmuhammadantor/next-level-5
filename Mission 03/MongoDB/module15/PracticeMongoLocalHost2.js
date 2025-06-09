1f09067bb9d2986366aa8bc37089a23be6566c69
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2025-06-09 17:52:34 +0600

    Practice Element Query Operators.

--- Mission 03/MongoDB/module15/PracticeMongoLocalHost2.js
+++ Mission 03/MongoDB/module15/PracticeMongoLocalHost2.js
@@ -31,3 +31,51 @@
 //     age: { $not: { $gt: 21 } }
 // }).projection({ name: 1, age: 1, email: 1, "address.country": 1 , _id: 1})
 //     .sort({ age: -1 }).limit(5)
+
+// db.practice.find({
+//     $nor: [{ "address.country": "China" }, { gender: "Female" }]
+// }).projection({ name: 1, age: 1, email: 1, "address.country": 1, gender: true, company: 1, friends: 1 })
+//     .sort({ age: -1 }).limit(5)
+
+
+// Module 15.6:
+// { email: "cthame2q@tumblr.com" }
+// db.practice.find({
+//     gender: {
+//         $exists: false
+//     }
+// })
+//     .projection({ name: 1, age: 1, email: 1, "address.country": 1, gender: true, company: 1, friends: 1 })
+//     .sort({})
+//     .limit(0)
+
+// db.practice.find({
+//     company: {
+//         $type: "null",
+//     }, friends: {
+//         $size: 0
+//     }
+// })
+//     .projection({ name: 1, age: 1, email: 1, "address.country": 1, gender: true, company: 1, friends: 1 })
+//     .sort({})
+// //     .limit(0)
+
+// db.practice.find({
+//     company: {
+//         $type: "null",
+//     }, friends: {
+//         $size: 0
+//     }
+// })
+//     .projection({ name: 1, age: 1, email: 1, "address.country": 1, gender: true, company: 1, friends: 1 })
+//     .sort({})
+//     .limit(0)
+
+// db.practice.find({
+//     friends: {
+//         $size: 5
+//     }
+// })
+//     .projection({ name: 1, age: 1, email: 1, "address.country": 1, gender: true, company: 1, friends: 1 })
+//     .sort({})
+//     .limit(0)
